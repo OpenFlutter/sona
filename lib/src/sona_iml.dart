cf145c358fd1746af1daa66d78d44a7e13806083
JarvanMo <jarvan.mo@gmail.com>
2018-09-10 16:46:41 +0800

    add register function

--- lib/src/sona_iml.dart
+++ lib/src/sona_iml.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/services.dart';
+import 'dart:async';
 
 final MethodChannel _channel = const MethodChannel('com.jarvanmo/sona');
+
+Future register() async{
+  return await _channel.invokeMethod("register","");
+}
